320dea335cdaee9e4b98ae32e98892f63913d9fd
Harry Buxton <hxb273@student.bham.ac.uk>
2023-12-02 17:00:07 +0000

    Day 2 completed

--- 2.2.js
+++ 2.2.js
@@ -4,33 +4,32 @@ var lineReader = require("line-reader");
 var sum = 0;
 lineReader.eachLine("./input2.txt", function (line, last) {
     var game = line.split(":");
-    var _a = game[0].split(" "), gameText = _a[0], gameNumber = _a[1];
     var set = game[1].split(";");
-    var impossible = false;
+    var minRed = 0;
+    var minBlue = 0;
+    var minGreen = 0;
     for (var i = 0; i < set.length; i++) {
         var colours = set[i].split(",");
         colours.forEach(function (colourAndNumber) {
             var _a = colourAndNumber.split(" "), filler = _a[0], number = _a[1], colour = _a[2];
             if (colour === "red") {
-                if (parseInt(number) > 12) {
-                    impossible = true;
+                if (parseInt(number) > minRed) {
+                    minRed = parseInt(number);
                 }
             }
             if (colour === "blue") {
-                if (parseInt(number) > 14) {
-                    impossible = true;
+                if (parseInt(number) > minBlue) {
+                    minBlue = parseInt(number);
                 }
             }
             if (colour === "green") {
-                if (parseInt(number) > 13) {
-                    impossible = true;
+                if (parseInt(number) > minGreen) {
+                    minGreen = parseInt(number);
                 }
             }
         });
     }
-    if (impossible === false) {
-        sum += parseInt(gameNumber);
-    }
+    sum += minRed * minBlue * minGreen;
     if (last) {
         console.log(sum);
     }
--- 2.2.ts
+++ 2.2.ts
@@ -4,34 +4,35 @@ let sum: number = 0;
 
 lineReader.eachLine("./input2.txt", (line: string, last: boolean) => {
   let game: string[] = line.split(":");
-  let [gameText, gameNumber] = game[0].split(" ");
   let set: string[] = game[1].split(";");
-  let impossible: boolean = false;
+
+  let minRed: number = 0;
+  let minBlue: number = 0;
+  let minGreen: number = 0;
 
   for (let i: number = 0; i < set.length; i++) {
     let colours = set[i].split(",");
     colours.forEach((colourAndNumber) => {
       const [filler, number, colour] = colourAndNumber.split(" ");
       if (colour === "red") {
-        if (parseInt(number) > 12) {
-          impossible = true;
+        if (parseInt(number) > minRed) {
+          minRed = parseInt(number);
         }
       }
       if (colour === "blue") {
-        if (parseInt(number) > 14) {
-          impossible = true;
+        if (parseInt(number) > minBlue) {
+          minBlue = parseInt(number);
         }
       }
       if (colour === "green") {
-        if (parseInt(number) > 13) {
-          impossible = true;
+        if (parseInt(number) > minGreen) {
+          minGreen = parseInt(number);
         }
       }
     });
   }
-  if (impossible === false) {
-    sum += parseInt(gameNumber);
-  }
+  sum += minRed * minBlue * minGreen;
+
   if (last) {
     console.log(sum);
   }
