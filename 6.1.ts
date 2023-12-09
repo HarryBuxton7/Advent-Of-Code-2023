85522f1cefabf239c1e2034ff40dd90dea8e529d
Harry Buxton <hxb273@student.bham.ac.uk>
2023-12-09 23:18:41 +0000

    day 6.1 completed

---
+++ 6.1.ts
@@ -0,0 +1,37 @@
+import { readFileSync } from "fs";
+
+function readfile(filename: string) {
+  const file = readFileSync(filename, "utf-8").trim();
+  const lines = file.split(/\r\n|\n/);
+  return lines;
+}
+
+main(readfile("example6.1.txt"));
+
+function main(linesArray) {
+  const firstLineNumbers = linesArray[0]
+    .split(" ")
+    .filter((e) => e.match("^[0-9]+$"))
+    .map((e) => parseInt(e));
+  const secondLineNumbers = linesArray[1]
+    .split(" ")
+    .filter((e) => e.match("^[0-9]+$"))
+    .map((e) => parseInt(e));
+  const races = [];
+  for (let i = 0; i < firstLineNumbers.length; i++) {
+    races.push([firstLineNumbers[i], secondLineNumbers[i]]);
+  }
+  let waysToWin = [];
+  for (const race of races) {
+    const [time, distance] = race;
+    let waysToWinCount = 0;
+    for (let i = 0; i < time; i++) {
+      let timeLeft = time - i;
+      if (timeLeft * i > distance) {
+        waysToWinCount++;
+      }
+    }
+    waysToWin.push(waysToWinCount);
+  }
+  console.log(waysToWin.reduce((a, b) => a * b, 1));
+}
