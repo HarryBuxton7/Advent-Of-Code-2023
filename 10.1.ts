6f52fea87ab1c8e14b9b0e4f4221f616220376ea
Harry Buxton <hxb273@student.bham.ac.uk>
2023-12-13 00:33:01 +0000

    day 10 part 1 completed

--- 10.1.ts
+++ 10.1.ts
@@ -15,27 +15,190 @@ function main(linesArray) {
     }
     mapGrid.push(charArray);
   }
-  const [currentYPosition, currentXPosition] = findStartingCoordinates(mapGrid);
-  let completedLoop = false
-  let stepsCount = 0
-  while (!completedLoop){
-    //check viability for each 8 directions (south, southwest, west, northwest, north, northeast, east, southeast)
-    //when checkViability returns true move currentPosition to that position
-    stepsCount++
+  let [currentPositionX, currentPositionY] = findStartingCoordinates(mapGrid);
+  let completedLoop = false;
+  let stepsCount = 0;
+  let previousPositionY: number;
+  let previousPositionX: number;
+  while (!completedLoop) {
+    stepsCount++;
+    console.log("while");
+    if (previousPositionY !== undefined) {
+      console.log(mapGrid[previousPositionY][previousPositionX]);
+    }
+    console.log(mapGrid[currentPositionY][currentPositionX]);
+
+    if (previousPositionY === undefined) {
+      switch (mapGrid[currentPositionY - 1][currentPositionX]) {
+        case "|":
+          previousPositionX = currentPositionX;
+          previousPositionY = currentPositionY;
+          currentPositionY -= 1;
+          break;
+        case "7":
+          previousPositionX = currentPositionX;
+          previousPositionY = currentPositionY;
+          currentPositionY -= 1;
+          break;
+
+        case "F":
+          previousPositionX = currentPositionX;
+          previousPositionY = currentPositionY;
+          currentPositionY -= 1;
+          break;
+      }
+      if (previousPositionX) {
+        continue;
+      }
+      switch (mapGrid[currentPositionY + 1][currentPositionX]) {
+        case "|":
+          previousPositionX = currentPositionX;
+          previousPositionY = currentPositionY;
+          currentPositionY += 1;
+          break;
+        case "L":
+          previousPositionX = currentPositionX;
+          previousPositionY = currentPositionY;
+          currentPositionY += 1;
+          break;
+
+        case "J":
+          previousPositionX = currentPositionX;
+          previousPositionY = currentPositionY;
+          currentPositionY += 1;
+          break;
+      }
+      if (previousPositionX) {
+        continue;
+      }
+      switch (mapGrid[currentPositionY][currentPositionX - 1]) {
+        case "-":
+          previousPositionX = currentPositionX;
+          previousPositionY = currentPositionY;
+          currentPositionX -= 1;
+          break;
+
+        case "F":
+          previousPositionX = currentPositionX;
+          previousPositionY = currentPositionY;
+          currentPositionX -= 1;
+          break;
+
+        case "L":
+          previousPositionX = currentPositionX;
+          previousPositionY = currentPositionY;
+          currentPositionX -= 1;
+          break;
+      }
+      if (previousPositionX) {
+        continue;
+      }
+      switch (mapGrid[currentPositionY][currentPositionX + 1]) {
+        case "-":
+          previousPositionX = currentPositionX;
+          previousPositionY = currentPositionY;
+          currentPositionX += 1;
+          break;
+
+        case "J":
+          previousPositionX = currentPositionX;
+          previousPositionY = currentPositionY;
+          currentPositionX += 1;
+          break;
+
+        case "7":
+          previousPositionX = currentPositionX;
+          previousPositionY = currentPositionY;
+          currentPositionX += 1;
+          break;
+      }
+      continue;
+    }
+
+    switch (mapGrid[currentPositionY][currentPositionX]) {
+      case "|":
+        if (currentPositionY + 1 === previousPositionY) {
+          previousPositionX = currentPositionX;
+          previousPositionY = currentPositionY;
+          currentPositionY -= 1;
+        } else {
+          previousPositionX = currentPositionX;
+          previousPositionY = currentPositionY;
+          currentPositionY += 1;
+        }
+        break;
+      case "-":
+        if (currentPositionX + 1 === previousPositionX) {
+          previousPositionX = currentPositionX;
+          previousPositionY = currentPositionY;
+          currentPositionX -= 1;
+        } else {
+          previousPositionX = currentPositionX;
+          previousPositionY = currentPositionY;
+          currentPositionX += 1;
+        }
+        break;
+      case "L":
+        if (currentPositionY - 1 === previousPositionY) {
+          previousPositionX = currentPositionX;
+          previousPositionY = currentPositionY;
+          currentPositionX += 1;
+        } else {
+          previousPositionX = currentPositionX;
+          previousPositionY = currentPositionY;
+          currentPositionY -= 1;
+        }
+        break;
+      case "J":
+        if (currentPositionY - 1 === previousPositionY) {
+          previousPositionX = currentPositionX;
+          previousPositionY = currentPositionY;
+          currentPositionX -= 1;
+        } else {
+          previousPositionX = currentPositionX;
+          previousPositionY = currentPositionY;
+          currentPositionY -= 1;
+        }
+        break;
+      case "7":
+        if (currentPositionY + 1 === previousPositionY) {
+          previousPositionX = currentPositionX;
+          previousPositionY = currentPositionY;
+          currentPositionX -= 1;
+        } else {
+          previousPositionX = currentPositionX;
+          previousPositionY = currentPositionY;
+          currentPositionY += 1;
+        }
+        break;
+      case "F":
+        if (currentPositionY + 1 === previousPositionY) {
+          previousPositionX = currentPositionX;
+          previousPositionY = currentPositionY;
+          currentPositionX += 1;
+        } else {
+          previousPositionX = currentPositionX;
+          previousPositionY = currentPositionY;
+          currentPositionY += 1;
+        }
+        break;
+    }
+    if (mapGrid[currentPositionY][currentPositionX] === "S") {
+      completedLoop = true;
+      break;
+    }
   }
+  console.log("steps count " + stepsCount / 2);
 }
 
 function findStartingCoordinates(mapGrid: string[][]) {
   for (let y = 0; y < mapGrid.length; y++) {
     for (let x = 0; x < mapGrid[y].length; x++) {
       if (mapGrid[y][x] === "S") {
-        return [y, x];
+        return [x, y];
       }
     }
   }
-}
-
-function checkViability(previousPosition: number[], newPosition: number[]) {
 }
 
 main(readfile("example10.txt"));
