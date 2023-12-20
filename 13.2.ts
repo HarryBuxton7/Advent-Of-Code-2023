60a3241180999763fcb4a78d47f3b9b4a5f76d84
Harry Buxton <hxb273@student.bham.ac.uk>
2023-12-20 14:30:35 +0000

    day 13 part 2 completed

---
+++ 13.2.ts
@@ -0,0 +1,96 @@
+import { readFileSync } from "fs";
+
+function readfile(filename: string) {
+  const file = readFileSync(filename, "utf-8").trim();
+  const lines = file.split(/\r\n|\n/);
+  return lines;
+}
+
+function main(linesArray) {
+  let ashArray = [];
+  let tempArray = [];
+  for (const line of linesArray) {
+    if (line.length === 0) {
+      ashArray.push(tempArray);
+      tempArray = [];
+      continue;
+    }
+    tempArray.push(line);
+  }
+  ashArray.push(tempArray);
+
+  let horizontalScore = 0;
+
+  for (const line of ashArray) {
+    console.log("//////////////////////////");
+    for (let i = 0; i < line.length - 1; i++) {
+      console.log(line[i], line[i + 1], "////");
+      let lowerReflection = line[i];
+      let upperReflection = line[i + 1];
+
+      for (let j = 1; j < line.length; j++) {
+        if (i - j >= 0 && i + j + 1 <= line.length - 1) {
+          lowerReflection += line[i - j];
+          upperReflection += line[i + j + 1];
+        }
+      }
+      if (lowerReflection !== upperReflection) {
+        let differenceCount = 0;
+        for (let i = 0; i < lowerReflection.length; i++) {
+          lowerReflection[i] !== upperReflection[i] && differenceCount++;
+        }
+        if (differenceCount === 1) {
+          horizontalScore += (i + 1) * 100;
+        }
+      }
+    }
+  }
+  console.log(horizontalScore);
+  const reversedAshArray = [];
+  for (const line of ashArray) {
+    reversedAshArray.push(inverseArray(line));
+  }
+
+  console.log("===========");
+  for (const line of reversedAshArray) {
+    console.log("//////////////////////////");
+    for (let i = 0; i < line.length - 1; i++) {
+      console.log(line[i], line[i + 1], "////");
+      let lowerReflection = line[i];
+      let upperReflection = line[i + 1];
+
+      for (let j = 1; j < line.length; j++) {
+        if (i - j >= 0 && i + j + 1 <= line.length - 1) {
+          lowerReflection += line[i - j];
+          upperReflection += line[i + j + 1];
+        }
+      }
+      if (lowerReflection !== upperReflection) {
+        let differenceCount = 0;
+        for (let i = 0; i < lowerReflection.length; i++) {
+          lowerReflection[i] !== upperReflection[i] && differenceCount++;
+        }
+        if (differenceCount === 1) {
+          console.log("true");
+          console.log(line[i], line[i + 1]);
+          horizontalScore += i + 1;
+        }
+      }
+    }
+  }
+  console.log(horizontalScore);
+}
+
+function inverseArray(originalArray: string[]) {
+  let inversedArray: string[] = [];
+  for (let i = 0; i < originalArray[0].length; i++) {
+    let tempString = "";
+    for (let j = 0; j < originalArray.length; j++) {
+      tempString += originalArray[j][i];
+    }
+    inversedArray.push(tempString);
+  }
+  return inversedArray;
+}
+
+main(readfile("example13.txt"));
